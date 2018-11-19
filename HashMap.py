4cfc96c0c5bfbebc193e2a12816c11af8fb045dd
Erik Rivera <erikrivera40@gmail.com>
2018-11-18 23:28:58 -0700

    Add files via upload

--- HashMap.py
+++ HashMap.py
@@ -1,24 +1,48 @@
+def string_to_num(word):
+        # This will convert a string to a positive integer
+        word = word.lower()
+
+        # Check if the word is empty
+        if word == "" or len(word)==0:
+            return 0
+        # Base case
+        if len(word) == 1:
+            # Return the unicode point value
+            return ord(word)-96
+        # return the value of the first character to the power of the length of the string times the value of the character
+        return string_to_num(word[1:]) +(26**(len(word)-1))*(ord(word[0])-96)
 class HashMap:
     def __init__(self, size):
+        self.size = size
         self.array = [None]*size
+        self.count = 0
     def __insert__(self, node):
         loc = node.value % self.size
-        if self.array[loc] == None:
-            self.array[loc] = node
-            return
-        node.next = self.array[loc]
-        self.array[loc] = node
+        self.array[loc] = HashNode(node.key, node.embedding, self.array[loc])
+        self.count+=1
         return
     def __insert_word_embedding__(self, word, embedding):
         node = HashNode(word,embedding)
         self.__insert__(node)
+        return
+    def __find__(self, word):
+        # find the value of the string and start iterating through the position
+        location = string_to_num(word)%self.size
+        temp_node = self.array[location]
+        while temp_node.key != None:
+            if temp_node.key == word:
+                return temp_node
+        print("Word: " + str(word) + " is not in the array...")
+        return None
 
-    def __find__(self, )
+        
 
 class HashNode:
     def __init__(self, key, embedding, next=None):
         # Key will be the actual word
         self.key = key
+        # Value will be converted from a base 26 to a base 10
+        self.value = string_to_num(self.key)
         # Embedding will hold the embedding for the node
         self.embedding = embedding
         # Next node
--- Lab4.py
+++ Lab4.py
@@ -1,13 +1,25 @@
+# Erik Rivera
+# Lab3A
+# CS2302 TR @ 10:30-11:50 
+# Professor Aguirre, Diego
+# TA Saha, Manoj
+# Date of last modification 11/9/2018
+# Purpose of this lab assigment is to improve running time of previous lab by using a hashmap
+# This lab uses word embeddings to get the similarity between to words
+
 from HashMap import HashMap
 from HashMap import HashNode
 
+import math
 import string
-def create_hash_array(file_name , size):
+
     
+def create_hash_array(file_name , size):
+    # Initiate the class
     hash_array = HashMap(size)
     # Try to open the file, in the case that an error occurs, inform the user and exit program
     try:
-        with open(file, encoding = "utf8") as file:
+        with open(file_name, encoding = "utf8") as file:
             alphabet = list(string.ascii_letters)
             # Scan each line
             for line in file:
@@ -17,17 +29,56 @@ def create_hash_array(file_name , size):
                 word = array[0]
                 # If the first letter of the word is in the alphabet then add to HashMap
                 if alphabet.__contains__(word[0]):
-                    embedding = array[1:]
+                    embedding = []
+                    for num in array[1:]:
+                        embedding.append(float(num))
                     node = HashNode(word, embedding)
+                    #print("Inserting: " + str(node.key))
                     hash_array.__insert__(node)
 
     except FileNotFoundError:
         print("Error, file was not found...")
+        print("Exiting...")
         exit()
     return hash_array
+def compare_from_word_file(array, file_name):
+    with open(file_name, encoding="utf8") as file:
+        for line in file:
+            words = line.split(" ")
+            first_node = array.__find__(words[0])
+            second_node = array.__find__(words[1])
+            similarity = compare_nodes(first_node, second_node)
+            print("Similarity between " + str(first_node.key) + " and " + str(second_node.key) + " is " + str(similarity))
+
+def compare_nodes(first_node, second_node):
+    # Check that both nodes contain embeddings
+    if first_node.embedding is not None and second_node.embedding is not None:
+        # Variable used for the top part of the fraction
+        top = 0
+        # Bottom variable used for bottom part of fraction
+        bottomA = 0
+        bottomB = 0
+        
+        for i in range(len(first_node.embedding)):
+            top = top + (first_node.embedding[i] * second_node.embedding[i])
+
+            bottomA = bottomA + first_node.embedding[i]**2
+            bottomB = bottomB + second_node.embedding[i]**2
+        return (top/(math.sqrt(bottomA) * math.sqrt(bottomB)))
+
+
 def main():
     # First read the file provided and save it into an array composed of HashNodes
-    size = input("Enter size of hash array\n")
-    hash_array = [None]*size
+    while(True):    
+        try:
+            size = int(input("Enter size of hash array\n"))
+            break
+        except ValueError:
+            print("Please enter a valid integer...")
+        
+    # Create the hash array by reading the file
     hash_array = create_hash_array(file_name , size)
-main
+    compare_from_word_file(hash_array, word_pairs_file_name)
+file_name = "glove.6B.50d.txt"
+word_pairs_file_name = "word_pairs.txt"
+main()
